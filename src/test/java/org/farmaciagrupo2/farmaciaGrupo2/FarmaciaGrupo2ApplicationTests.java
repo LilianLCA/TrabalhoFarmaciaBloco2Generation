7319214855dd29808669bbf5280b08776a159eea
muriloperdigao <murilo.apm@gmail.com>
2022-01-26 11:02:54 -0300

    Model 26.01.2022

---
+++ src/test/java/org/farmaciagrupo2/farmaciaGrupo2/FarmaciaGrupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.farmaciagrupo2.farmaciaGrupo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaGrupo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
